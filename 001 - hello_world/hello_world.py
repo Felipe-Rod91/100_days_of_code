f040fd51b93c8933a6810a61ededdf61a7188646
Felipe Rodrigues <felipe.git@outlook.com>
2024-02-16 12:04:44 -0300

    Primeiro Commit

---
+++ 001 - hello_world/hello_world.py
@@ -0,0 +1,8 @@
+# Crie uma função que cumprimente um usuário e que deseje sorte nessa jornada
+
+# Função para definir a mensagem de boas vindas
+def hello_world(name):
+    print(f'Olá {name}! Vamos juntos nessa jornada!')
+
+# Chamada da função com o nome desejado
+hello_world('Felipe')
