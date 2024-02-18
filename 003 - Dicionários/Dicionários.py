0896a8bad01645434cb8718bc5bd0dc29b755477
Felipe Rodrigues <felipe.git@outlook.com>
2024-02-18 13:09:54 -0300

    Sistema básico que define o nome do jogador, número de partidas e de gols, tanto em cada partida quanto no total

--- 003 - Dicionários/Dicionários.py
+++ 003 - Dicionários/Dicionários.py
@@ -3,9 +3,17 @@
 # durante o campeonato. Aprimore o desafio para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamento 
 # de cada jogador.
 
+# Criação do dicionário com nome do jogador, partidas e gols
 jogador = dict()
 jogador['nome'] = str(input('Nome do jogador: '))
 jogador['partidas'] = int(input('Quantas partidas jogou? '))
+jogador['gols'] = list()
+
+# Sistema que define, pelo número de partidas, quantos gols marcou em cada e o total da temporada, colocando cada número dentro de uma lista dentro do dicionário 
+# do jogador
+for partida in range(jogador['partidas']):
+    gol = int(input(f'Quantos gols marcou na {partida+1}ª partida? '))
+    jogador['gols'].append(gol)
+    jogador['total_gols'] = sum(jogador['gols'])
+
 print(jogador)
-
-
