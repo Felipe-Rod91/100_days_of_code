87857e7afbadd396d081ade261ab79128dae5c00
Felipe Rodrigues <felipe.git@outlook.com>
2024-02-18 19:03:15 -0300

    Finalização do projeto

--- 003 - Dicionários/Dicionários.py
+++ 003 - Dicionários/Dicionários.py
@@ -1,7 +1,7 @@
 # Crie um programa que gerencie o aproveitamento de um jogador de futebol. O programa vai ler o nome do jogador e quantas partidas ele jogou. 
 # Depois vai ler a quantidade de gols feitos em cada partida. No final, tudo isso será guardado em um dicionário, incluindo o total de gols feitos 
-# durante o campeonato. Aprimore o desafio para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamento 
-# de cada jogador.
+# durante o campeonato. Aprimore o desafio para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do 
+# aproveitamento de cada jogador.
 
 import time
 
