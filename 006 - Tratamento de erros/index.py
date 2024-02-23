eff3dfc4ffb450e2b48bc589fd3cb2c006707089
Felipe Rodrigues <felipe.git@outlook.com>
2024-02-22 23:09:58 -0300

    Inseridas as cores e criados os módulos e pacotes

--- 006 - Tratamento de erros/index.py
+++ 006 - Tratamento de erros/index.py
@@ -3,6 +3,15 @@
 
 from time import sleep
 
+# Cores
+white = '\033[30m'
+red = '\033[31m'
+green = '\033[32m'
+yellow = '\033[33m'
+blue = '\033[34m'
+neutral = '\033[m'
+
+
 # Função que define a estética dos títulos de cada função
 def title(msg):
     print('-' * 40)
@@ -12,9 +21,9 @@ def title(msg):
 # Função de menu principal, com o título da função Title()
 def main_menu():
     title('MENU PRINCIPAL')
-    print('1- Visualizar pessoas cadastradas')
-    print('2- Cadastrar nova pessoa')
-    print('3- Sair do programa')
+    print(f'{yellow}1-{neutral} {blue}Visualizar pessoas cadastradas{neutral}')
+    print(f'{yellow}2-{neutral} {blue}Cadastrar nova pessoa{neutral}')
+    print(f'{yellow}3-{neutral} {blue}Sair do programa{neutral}')
     print('-' * 40)
 
 # Função para escolher a opção certa e o tratamento de erros
@@ -23,25 +32,33 @@ def options(msg):
         try:
             num = int(input(msg))
         except (ValueError, TypeError):
-            print('Valor inválido! Tente novamente.')
+            print(f'{red}Valor inválido! Tente novamente.{neutral}')
             continue
+        except KeyboardInterrupt:
+            print(f'{red}Valor não encontrado. Digite um valor.{neutral}')
         else:
             return num
 
 # Função da opção 1, de visualizar as pessoas cadastradas
 def option_1():
     title('OPÇÃO 1')
-    sleep(3)
+    sleep(2)
 
 # Função da opção 2, de cadastrar nova pessoa
 def option_2():
     title('OPÇÃO 2')
-    sleep(3)
+    name = str(input('Nome: '))
+    age = int(input('Idade: '))
+    print('Armazenando os dados...', end=' ')
+    sleep(2)
+    print('PRONTO!')
+    sleep(2)
+
 
 # Programa principal
 while True:
     main_menu()
-    option = options('Digite um valor: ')
+    option = options(f'{green}Digite um valor: {neutral}')
     if option == 1:
         option_1()
     elif option == 2:
@@ -49,7 +66,7 @@ while True:
     elif option == 3:
         break
     else:
-        print('Valor não encontrado. Digite um número válido.')
+        print(f'{red}Valor não encontrado. Digite um número válido.{neutral}')
 
 # Mensagem de programa encerrado
 title('PROGRAMA ENCERRADO')
