2dd73402b0152b0363eec6de7ed4ee56fe986b84
Felipe Rodrigues <felipe.git@outlook.com>
2024-02-23 18:46:37 -0300

    Funções de formatação passadas para o pacote de interface

--- 006 - Tratamento de erros/index.py
+++ 006 - Tratamento de erros/index.py
@@ -1,29 +1,8 @@
 # Vamos criar um menu em Python usando modularização, onde existem as funções de cadastrar pessoas, visualizar
-# as pessoas cadastradas e sair do programa
+# as pessoas cadastradas e sair do programa.
 
 from time import sleep
-
-# Cores
-white = '\033[30m'
-red = '\033[31m'
-green = '\033[32m'
-yellow = '\033[33m'
-blue = '\033[34m'
-neutral = '\033[m'
-
-# Função que define a estética dos títulos de cada função
-def title(msg):
-    print('-' * 40)
-    print(f'{msg:^40}')
-    print('-' * 40)
-
-# Função de menu principal, com o título da função Title()
-def main_menu():
-    title('MENU PRINCIPAL')
-    print(f'{yellow}1-{neutral} {blue}Visualizar pessoas cadastradas{neutral}')
-    print(f'{yellow}2-{neutral} {blue}Cadastrar nova pessoa{neutral}')
-    print(f'{yellow}3-{neutral} {blue}Sair do programa{neutral}')
-    print('-' * 40) 
+from interface import format
 
 # Função para escolher a opção certa e o tratamento de erros
 def options(msg):
@@ -31,16 +10,16 @@ def options(msg):
         try:
             num = int(input(msg))
         except (ValueError, TypeError):
-            print(f'{red}Valor inválido! Tente novamente.{neutral}')
+            print(f'{format.red}Valor inválido! Tente novamente.{format.neutral}')
             continue
         except KeyboardInterrupt:
-            print(f'{red}Valor não encontrado. Digite um valor.{neutral}')
+            print(f'{format.red}Valor não encontrado. Digite um valor.{format.neutral}')
         else:
             return num
 
 # Função da opção 1, de visualizar as pessoas cadastradas
 def registered_people():
-    title('PESSOAS CADASTRADAS')
+    format.title('PESSOAS CADASTRADAS')
     print(f'{"NOME":<35}{"IDADE"}')
     for person in people:
         print(f'{person['nome']:<35}{person['idade']:^5}')
@@ -48,7 +27,7 @@ def registered_people():
 
 # Função da opção 2, de cadastrar nova pessoa
 def register():
-    title('CADASTRO DE PESSOAS')
+    format.title('CADASTRO DE PESSOAS')
     while True:
         try:
             person['nome'] = str(input('Nome: '))
@@ -61,21 +40,21 @@ def register():
         try:
             person['idade'] = int(input('Idade: '))
         except (ValueError, TypeError):
-            print(f'{red}Valor inválido! Digite um número inteiro.{neutral}')
+            print(f'{format.red}Valor inválido! Digite um número inteiro.{format.neutral}')
             continue
         else:
             break
     people.append(person.copy())  
-    print(f'{green}Armazenando os dados...', end=' ')
+    print(f'{format.green}Armazenando os dados...', end=' ')
     sleep(2)
-    print(f'PRONTO!{neutral}')
+    print(f'PRONTO!{format.neutral}')
     sleep(2)
 
 # Programa principal
 person = dict()
 people = list()
 while True:
-    main_menu()
+    format.main_menu()
     option = options(f'Digite um valor: ')
     if option == 1:
         registered_people()
@@ -84,7 +63,7 @@ while True:
     elif option == 3:
         break
     else:
-        print(f'{red}Valor não encontrado. Digite um número válido.{neutral}')
+        print(f'{format.red}Valor não encontrado. Digite um número válido.{format.neutral}')
 
 # Mensagem de programa encerrado
-title('PROGRAMA ENCERRADO')
+format.title('PROGRAMA ENCERRADO')
