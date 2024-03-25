f01dd2c242a55feb486aa563dffd97211adbe4d0
Felipe Rodrigues <felipe.git@outlook.com>
2024-03-25 02:06:54 -0300

    Treinando conceitos de herança em POO

--- 008_variaveis_de_ambiente/api.py
+++
@@ -1,7 +0,0 @@
-# Função que verifica usuário e senha. Se forem corretos, ocorre login.
-# Caso contrário, não ocorre login.
-def login(usuario, senha):
-    if usuario == 'felipe.rod' and senha == 'pastel':
-        print('Usuário e senha CORRETOS, você está logado.')
-    else:
-        print('Usuário e/ou senha INCORRETOS, você NÃO está logado.')
